a65ebbf9dd86a6e2762048b2a2cafa3d1ed18583
Gérémy Lubin <97472153+lubingeremy@users.noreply.github.com>
2022-06-08 12:00:12 -0400

    Project page responsive

--- pages/projects/[projectSlug].js
+++ pages/projects/[projectSlug].js
@@ -35,48 +35,52 @@ export default function Home ({ fileName, projectSlug, }) {
 		<>
 			{ProjectsData.map((project, index) => {
 				return (
-					<article key={project.url} className={`${project.url === projectSlug ? "" : "hidden"} flex flex-row bg-charcoal`}>
-						<Link href="/#projects">
-							<a className="w-1/12 h-max px-1 md:w-32 md:pl-4 pt-5">
-								<div className='md:sticky md:top-8'>
-									<Image 
-										src={backArrow}
-										width="34"
-										height="22"
+					<article key={project.url} className={`${project.url === projectSlug ? "" : "hidden"} flex flex-row bg-charcoal pb-16 md:pb-0`}>
+						<div className='w-1/6 sticky top-10 h-max md:w-28 flex justify-center items-center p-2'>
+							<Link href="/#projects">
+								<a className="p-3">
+									<div className=''>
+										<Image 
+											src={backArrow}
+											width="34"
+											height="22"
+											className=""
+											alt=''
+										/>
+									</div>
+								</a>
+							</Link>
+						</div>
+						<div className='w-full flex flex-col pr-6 md:pr-0 md:gap-6 md:flex-row'>
+							<div className='w-full md:h-screen flex justify-start md:justify-center items-center py-12 md:py-24 '>
+								<div className="w-1/2 md:hidden">
+									<Image
+										src={require('../../public/' + project.imageMobile)}
+										alt=""
+										height="207"
+										width="119"
+										layout="responsive"
+										priority=""
 										className=""
-										alt=''
 									/>
 								</div>
-							</a>
-						</Link>
-						<div className='w-full h-screen flex justify-center items-center px-2 py-24 '>
-							<div className="w-11/12 md:hidden">
-								<Image
-									src={require('../../public/' + project.imageMobile)}
-									alt=""
-									height="207"
-									width="119"
-									layout="responsive"
-									priority=""
-									className=""
-								/>
+								<div className="hidden md:block md:w-11/12">
+									<Image
+										src={require('../../public/' + project.image)}
+										alt=""
+										height="347"
+										width="525"
+										layout="responsive"
+										className=""
+									/>
+								</div>
 							</div>
-							<div className="hidden md:block md:w-11/12">
-								<Image
-									src={require('../../public/' + project.image)}
-									alt=""
-									height="347"
-									width="525"
-									layout="responsive"
-									className=""
-								/>
+							<div className='md:w-8/12 md:h-screen overflow-auto flex flex-col gap-5 md:gap-10 md:pr-24 md:py-24'> 
+								<h1 className='text-6xl'>{project.title}</h1>
+								<p className='text-base'>{project.tech}</p>
+								<p className='text-lg text-justify'>{project.description}</p>
+								<p className='text-lg text-justify'>{project.description}</p>
 							</div>
-						</div>
-						<div className='w-7/12 h-screen overflow-auto flex flex-col gap-10 px-12 py-24'> 
-							<h1 className='text-6xl'>{project.title}</h1>
-							<p className='text-base'>{project.tech}</p>
-							<p className='text-lg text-justify'>{project.description}</p>
-							<p className='text-lg text-justify'>{project.description}</p>
 						</div>
 					</article>
 				)
